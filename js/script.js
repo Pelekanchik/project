0dcaebb144a2f8d34efde611ea575d6e25d0a360
tolik <example@gmail.com>
2026-05-09 18:01:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 13;
+// console.log(number);
+// const answers = [];
+
+// answers[0] = prompt('Як твоє імя?', '');
+// answers[1] = prompt('Як твоя фамілія?', '');
+// answers[2] = prompt('Скільки тобі років?', '');
+
+// document.write(answers);
+console.log('rety ' + 'retyu');
+
+let arr = 12;
+let tuu = 13;
+
+console.log(arr++);
+console.log(arr);
